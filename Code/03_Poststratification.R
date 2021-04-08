d86693e57af840de2de4f09cbd78c316321e0b62
blobo184 <54280196+blobo184@users.noreply.github.com>
2021-04-08 12:37:32 +0100

    Update 03_Poststratification.R

--- Code/03_Poststratification.R
+++ Code/03_Poststratification.R
@@ -15,25 +15,25 @@ load("../Data/post.RData")
 
 ## Postrat and save for baseline model (no power priors)
 mod <- readRDS("../Results/baseline_mod.rds")
-result_export <- stan_poststrat(mod, post)
+result_export <- stan_poststrat(mod, post, iter =100)
 save(result_export, file = "../Results/baseline_results.RData")
 
 
 ## Postrat and save for scaling constant 0.5 model
 mod <- readRDS("../Results/mod_pp_0.5.rds")
-result_export <- stan_poststrat(mod, post)
+result_export <- stan_poststrat(mod, post, iter =100)
 save(result_export, file = "../Results/pp_0.5.RData")
 
 
 ## Postrat and save for scaling constant 0.25 model
 mod <- readRDS("../Results/mod_pp_0.25.RDS")
-result_export <- stan_poststrat(mod, post)
+result_export <- stan_poststrat(mod, post, iter =100)
 save(result_export, file = "../Results/pp_0.25.RData")
 
 
 ## Postrat and save for scaling constant 0.1 model
 mod <- readRDS("../Results/mod_pp_0.1.RDS")
-result_export <- stan_poststrat(mod, post)
+result_export <- stan_poststrat(mod, post, iter =100)
 save(result_export, file = "../Results/pp_0.1.RData")
 
 ####################### ENd #############################
